ee635291a8831eeebec20ab745c783b700edc4c2
persikfloro <liberavivi888@gmail.com>
2023-03-14 19:00:12 +0300

    initial commit

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -1,9 +1,8 @@
 package ru.netology.delivery.test;
 
-import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
@@ -11,13 +10,19 @@ import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static ru.netology.delivery.data.DataGenerator.generateDate;
 
 class DeliveryTest {
-
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
@@ -53,4 +58,24 @@ class DeliveryTest {
                 .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate))
                 .shouldBe(visible);
     }
+
+    @Test
+    @DisplayName("Should not plan meeting")
+    void shouldNotPlanMeeting() {
+        var validUser = DataGenerator.Registration.generateUser("en");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = generateDate(daysToAddForFirstMeeting);
+        $("[data-test-id=city] .input__control").setValue(validUser.getCity());
+        $("[data-test-id=date] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id = date] .input__control").setValue(firstMeetingDate);
+        $("[data-test-id = name] .input__control").setValue(validUser.getName());
+        $("[data-test-id = phone] .input__control").setValue(validUser.getPhone());
+        $("[data-test-id = agreement]").click();
+        $(byText("Запланировать")).click();
+        $(byText("Доставка в выбранный город недоступна")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=date] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+
+    }
+
+
 }
